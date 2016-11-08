c6475398af836702c100214257ba422fcd039d1d
byplacebo <placebo2me@gmail.com>
2016-11-08 21:09:40 +0900

    refactoring for example without comments about javacafe

---
+++ hello-web-security/src/main/java/me/placebo/spring/security/example/AuthenticationExample.java
@@ -0,0 +1,39 @@
+package me.placebo.spring.security.example;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author 임형태
+ * @since 2015.10.28
+ */
+public class AuthenticationExample {
+    private static AuthenticationManager authenticationManager = new SampleAuthenticationManager();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.println("Please enter your username:");
+            String name = in.readLine();
+            System.out.println("Please enter your password:");
+            String password = in.readLine();
+            try {
+                Authentication request = new UsernamePasswordAuthenticationToken(name, password);
+                Authentication result = authenticationManager.authenticate(request);
+                SecurityContextHolder.getContext().setAuthentication(result);
+                break;
+            } catch (AuthenticationException e) {
+                System.out.println("Authentication failed: " + e.getMessage());
+            }
+        }
+        System.out.println("Successfully authenticated. Security context contains: " + SecurityContextHolder.getContext().getAuthentication());
+    }
+}
+
+
